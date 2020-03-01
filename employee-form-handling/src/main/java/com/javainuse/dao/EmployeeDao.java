b75ad82e0fc40c7fdf524b833306c29ed0c410c2
Dhruva Narayan <54905394+dhruvix@users.noreply.github.com>
2020-03-01 12:58:09 +0530

    Add files via upload

---
+++ employee-form-handling/src/main/java/com/javainuse/dao/EmployeeDao.java
@@ -0,0 +1,9 @@
+package com.javainuse.dao;
+import java.util.List;
+
+import com.javainuse.model.Employee;
+
+public interface EmployeeDao {
+	void insertEmployee(Employee cus);
+	List<Employee> getAllEmployees();
+}
